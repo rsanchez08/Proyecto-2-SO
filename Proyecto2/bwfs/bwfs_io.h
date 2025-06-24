a8a4354ec1a54acd6c9a3cfd2992b686a4c1d351
René Sánchez <65914423+rsanchez08@users.noreply.github.com>
2025-06-23 22:50:04 -0600

    Delete Proyecto2 directory

--- Proyecto2/bwfs/bwfs_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef BWFS_IO_H
-#define BWFS_IO_H
-
-#include "bwfs.h"
-
-int bwfs_save_image(const char *path, bwfs_superblock *sb);
-int bwfs_load_image(const char *path, bwfs_superblock *sb);
-
-#endif
